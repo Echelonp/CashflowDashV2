ddfe7f584717c944a0cce9e80b0b961975ab6177
Paradorn Pimporn <paraz@Paradorns-MacBook-Pro.local>
2020-05-13 21:59:09 +0700

    New style for policy

--- src/components/dashboard/cashFlowSensitivity/Policy.js
+++ src/components/dashboard/cashFlowSensitivity/Policy.js
@@ -4,19 +4,21 @@ const Policy = (props) => {
   //   console.log("Policy", props);
   const policyList = props.policyList.map((policy, i) => {
     return (
-      <div className="left-align" key={policy.id}>
+      <li className="left-align" key={policy.id}>
         <div>
+          <span style={{ fontSize: "18px" }}>{policy.namefull}</span>
+          <br></br>
           <label>
             <input
               type="checkbox"
               className="filled-in"
               onChange={(e) => props.policyApply(policy.id, e.target.checked)}
             ></input>
-            <span style={{ fontSize: "18px" }}>{policy.namefull}</span>
+            <span style={{ fontSize: "18px" }}>ทดสอบ</span>
           </label>
         </div>
         <br></br>
-      </div>
+      </li>
     );
   });
 
@@ -25,7 +27,7 @@ const Policy = (props) => {
       <span style={{ fontSize: "20px" }}>
         <strong>มาตราการ</strong>
       </span>
-      {policyList}
+      <ol>{policyList}</ol>
     </div>
   );
 };
